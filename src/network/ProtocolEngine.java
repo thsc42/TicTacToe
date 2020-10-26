26d95eff9770290e678efb46b390485915553844
thsc42 <thomas.schwotzer@htw-berlin.de>
2020-10-26 17:05:01 +0100

    protocol engine has got threads now

--- src/network/ProtocolEngine.java
+++ src/network/ProtocolEngine.java
@@ -5,5 +5,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface ProtocolEngine {
+    /**
+     * Setup protocol engine. Provide streams to read from and write to.
+     * @param is
+     * @param os
+     * @throws IOException
+     */
     void handleConnection(InputStream is, OutputStream os) throws IOException;
 }
